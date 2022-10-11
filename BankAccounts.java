412dd3dfb7fae5f367038157221a496402d08019
nolliechyTW <106467497+nolliechyTW@users.noreply.github.com>
2022-10-11 11:59:11 +0800

    Update BankAccounts.java

--- BankAccounts.java
+++ BankAccounts.java
@@ -28,19 +28,19 @@ public class BankAccount {
 
 		/**
 		 * Create a bank account of given type for given customer
-     * @param accountType for bank account
-     * @param customer for bank account
+ 		 * @param accountType for bank account
+    		 * @param customer for bank account
 		 **/
-    public BankAccount(String accountType, Customer customer) {
-				this.accountType = accountType;
-				this.customer = customer;
+		public BankAccount(String accountType, Customer customer) {
+			this.accountType = accountType;
+			this.customer = customer;
 		}
 
 		//method
 
 		/**
 		 * Deposit/withdraw the given amount
-     * @param amount to add/reduce to balance
+     		 * @param amount to add/reduce to balance
 		 * @throws Exception if given amount is larger than available balance
 		 **/
 		public void deposit(double amount) {
@@ -48,30 +48,29 @@ public class BankAccount {
 		}
 
 		public void withdraw(double amount) throws Exception {
-				if (amount > this.balance( {
-						throw new Exception("Amount is greater than available balance."); 
+			if (amount > this.balance) {
+			throw new Exception("Amount is greater than available balance."); 
 			//Exception is a built-in class, which indicates that a problem occurred.
-				}
-				this.balance -= amount;
+			}
+			this.balance -= amount;
 		}
 
 
 		/**
 		 * return account type and balance for this bank account
-     * @return string with all the info
+    		 * @return string with all the info
 		 **/
 		public String getAccountInfo() {
-				return this.accounType +": " + this.balance;
+			return this.accounType +": " + this.balance;
 		}
-
 
 		/**
 		 * return the customer name and address for this bank account
-     * @return string with all the info
+    		 * @return string with all the info
 		 **/
 
 		public String getCustomerInfo() {
-				return this.customer.getName +" from " + this.customer.getAddress;
+			return this.customer.getName +" from " + this.customer.getAddress;
 		}
 
 } 
